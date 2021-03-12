651798c67f16d598bbc001eeb5d3b575c8bed0cf
rahulzeeweesoft <zeeweesoft.rahul@gmail.com>
2021-03-12 16:49:22 +0530

    ssr code

--- server/index.js
+++ server/index.js
@@ -8,14 +8,16 @@ const router = express.Router();
 const app = express();
 
   
-  // app.use('/',router)
-  // require('./server')(router)
+  app.use('/',router)
+  require('./server')(router)
   
-  app.get('/*', (req, res) => {
-    add.add(req ,res)
-  });
+  // app.get('/*', (req, res) => {
+  //   // console.log(req.headers.referer)
+  //   add.add(req ,res)
+  // });
 
   app.use(express.static('./build'));
+  app.use(express.static('./assets'));
 
 
   app.listen(PORT, () => {
--- server/server.js
+++ server/server.js
@@ -26,11 +26,22 @@ app.get('/process-automation', (req, res) => {
     add.add(req ,res)
 });
 
+app.get('/finance', (req, res) => {
+    add.add(req ,res)
+});
+
+app.get('/finance', (req, res) => {
+    add.add(req ,res)
+});
+
 app.get('/data-lake', (req, res) => {
     add.add(req ,res)
 });
 
 app.get('/data-engineering', (req, res) => {
+    add.add(req ,res)
+});
+app.get('/engineering', (req, res) => {
     add.add(req ,res)
 });
 
@@ -45,7 +56,7 @@ app.get('/improved-customer-retention', (req, res) => {
     add.add(req ,res)
 });
 
-app.get( '/chatbots', (req, res) => {
+app.get('/chatbots', (req, res) => {
     add.add(req ,res)
 });
 
@@ -101,11 +112,11 @@ app.get('/conversational-support', (req, res) => {
     add.add(req ,res)
 });
 
-app.get(' /demand-forecasting', (req, res) => {
+app.get('/demand-forecasting', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/incident-analyzer-predictor ', (req, res) => {
+app.get('/incident-analyzer-predictor', (req, res) => {
     add.add(req ,res)
 });
 
@@ -116,74 +127,74 @@ app.get('/transpPlatform', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/pandemic-crowd-control ', (req, res) => {
+app.get('/pandemic-crowd-control', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/predictive-maintenance ', (req, res) => {
+app.get('/predictive-maintenance', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/air-pollution-monitoring-prediction ', (req, res) => {
+app.get('/air-pollution-monitoring-prediction', (req, res) => {
     add.add(req ,res)
 });
 app.get('/industries', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/banking ', (req, res) => {
+app.get('/banking', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('healthcare ', (req, res) => {
+app.get('/healthcare', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/hightech ', (req, res) => {
+app.get('/hightech', (req, res) => {
     add.add(req ,res)
 });
 app.get('/retail', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/oilgas ', (req, res) => {
+app.get('/oilgas', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/utility ', (req, res) => {
+app.get('/utility', (req, res) => {
     add.add(req ,res)
 });
 
 app.get('/transpAndLogistic', (req, res) => {
     add.add(req ,res)
 });
-app.get('/environmental ', (req, res) => {
+app.get('/environmental', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/ai-powered-processing ', (req, res) => {
+app.get('/ai-powered-processing', (req, res) => {
     add.add(req ,res)
 });
 app.get('/business-process', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/clinical-trial ', (req, res) => {
+app.get('/clinical-trial', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/customer-engage ', (req, res) => {
+app.get('/customer-engage', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/customer-insights ', (req, res) => {
+app.get('/customer-insights', (req, res) => {
     add.add(req ,res)
 });
 app.get('/data-driven', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/decision-supports ', (req, res) => {
+app.get('/decision-supports', (req, res) => {
     add.add(req ,res)
 });
 
@@ -191,7 +202,7 @@ app.get('/document-processing', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/implement-healthcare ', (req, res) => {
+app.get('/implement-healthcare', (req, res) => {
     add.add(req ,res)
 });
 app.get('/enterprise', (req, res) => {
@@ -206,14 +217,14 @@ app.get('/privacy-policy', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/real-time ', (req, res) => {
+app.get('/real-time', (req, res) => {
     add.add(req ,res)
 });
 app.get('/reduction-not-performing', (req, res) => {
     add.add(req ,res)
 });
 
-app.get(' /resp-research-dev', (req, res) => {
+app.get('/resp-research-dev', (req, res) => {
     add.add(req ,res)
 });
 
@@ -221,36 +232,36 @@ app.get('/smart-cheque', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/smart-optimization ', (req, res) => {
+app.get('/smart-optimization', (req, res) => {
     add.add(req ,res)
 });
 app.get('/social-media', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/software-services ', (req, res) => {
+app.get('/software-services', (req, res) => {
     add.add(req ,res)
 });
 
-app.get(' /solution', (req, res) => {
+app.get('/solution', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/xml-data ', (req, res) => {
+app.get('/xml-data', (req, res) => {
     add.add(req ,res)
 });
-app.get(' /ai-powered', (req, res) => {
+app.get('/ai-powered', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/improved-customer-retention ', (req, res) => {
+app.get('/improved-customer-retention', (req, res) => {
     add.add(req ,res)
 });
 app.get('/travel-hospitality', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/it-ites ', (req, res) => {
+app.get('/it-ites', (req, res) => {
     add.add(req ,res)
 });
 
@@ -258,44 +269,44 @@ app.get('/marketing', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/human-resource ', (req, res) => {
+app.get('/human-resource', (req, res) => {
     add.add(req ,res)
 });
 app.get('/safety-security', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/ai-powered-bot ', (req, res) => {
+app.get('/ai-powered-bot', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/risk-assesment ', (req, res) => {
+app.get('/risk-assesment', (req, res) => {
     add.add(req ,res)
 });
 
-app.get(' /Digital-outreach', (req, res) => {
+app.get('/Digital-outreach', (req, res) => {
     add.add(req ,res)
 });
 app.get('/customer-segment', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/employee-attrition-retain ', (req, res) => {
+app.get('/employee-attrition-retain', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/employee-engagement ', (req, res) => {
+app.get('/employee-engagement', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/employee-productivity ', (req, res) => {
+app.get('/employee-productivity', (req, res) => {
     add.add(req ,res)
 });
 app.get('/retail-case-studies', (req, res) => {
     add.add(req ,res)
 });
 
-app.get(' /collaborative-healthcare', (req, res) => {
+app.get('/collaborative-healthcare', (req, res) => {
     add.add(req ,res)
 });
 
@@ -303,29 +314,29 @@ app.get('/population-healthcare', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/preventive-healthcare ', (req, res) => {
+app.get('/preventive-healthcare', (req, res) => {
     add.add(req ,res)
 });
 app.get('/virtual-clinic', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/disease-prognosis-assessment-platform ', (req, res) => {
+app.get('/disease-prognosis-assessment-platform', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/precision-medicine-platform-adma ', (req, res) => {
+app.get('/precision-medicine-platform-adma', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/precision-medicine-platform-ecosystem ', (req, res) => {
+app.get('/precision-medicine-platform-ecosystem', (req, res) => {
     add.add(req ,res)
 });
 app.get('/precision-medicine-platform-propotion', (req, res) => {
     add.add(req ,res)
 });
 
-app.get(' /price-optimization-engine', (req, res) => {
+app.get('/price-optimization-engine', (req, res) => {
     add.add(req ,res)
 });
 app.get('/non-performing-asset-prediction', (req, res) => {
@@ -336,7 +347,7 @@ app.get('/KYC_AML-custom-on-boarding', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/AI-powered-prospect-targeting ', (req, res) => {
+app.get('/AI-powered-prospect-targeting', (req, res) => {
     add.add(req ,res)
 });
 
@@ -347,7 +358,7 @@ app.get('/international-arbitrage-trading', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/smart-transport-solution ', (req, res) => {
+app.get('/smart-transport-solution', (req, res) => {
     add.add(req ,res)
 });
 
@@ -362,30 +373,30 @@ app.get('/adobe-case-study', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/transport-route-optimization ', (req, res) => {
+app.get('/transport-route-optimization', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/insurance-policy-renewability ', (req, res) => {
+app.get('/insurance-policy-renewability', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/sales ', (req, res) => {
+app.get('/sales', (req, res) => {
     add.add(req ,res)
 });
 app.get('/supply-chain', (req, res) => {
     add.add(req ,res)
 });
 
-app.get(' /success', (req, res) => {
+app.get('/success', (req, res) => {
     add.add(req ,res)
 });
 
-app.get('/about#contactus ', (req, res) => {
+app.get('/about#contactus', (req, res) => {
     add.add(req ,res)
 });
 
-app.get(' /Ar-vr', (req, res) => {
+app.get('/Ar-vr', (req, res) => {
     add.add(req ,res)
 });
 
